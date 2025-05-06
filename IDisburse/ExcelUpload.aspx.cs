8c595ef2469fd5b88bcf94e40eb47d9efedf6d66
rvsmask <raghvendra.mask2@gmail.com>
2025-05-06 08:35:43 +0530

    Update ExcelUpload.aspx.cs
    changed oledb

--- IDisburse/ExcelUpload.aspx.cs
+++ IDisburse/ExcelUpload.aspx.cs
@@ -1,4 +1,4 @@
-﻿using OfficeOpenXml;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -26,6 +26,8 @@ namespace IDisburse
 
         protected void btnExclUpload_Click(object sender, EventArgs e)
         {
+            string filePath = string.Empty;
+
             if (fileExcel.HasFile)
             {
                 string FileExtension = System.IO.Path.GetExtension(fileExcel.FileName);
@@ -35,67 +37,43 @@ namespace IDisburse
                     string strFileName = DateTime.Now.Day.ToString() + '_' + DateTime.Now.Month.ToString() + '_' + DateTime.Now.Year.ToString() + '_' + DateTime.Now.Hour.ToString() + '_' +
                                          DateTime.Now.Minute.ToString() + '_' + fileExcel.FileName.ToString();
 
-                    string filePath = Server.MapPath("~/Upload/" + strFileName);
+                     filePath = Server.MapPath("~/Upload/" + strFileName);
                     fileExcel.SaveAs(filePath);
 
-                    try
+
+                    if (File.Exists(filePath))
                     {
-                        using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
-                        {
-                            // Licence for Non-Commercial applications
-                            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
-
-                            // Assuming the data is in the first worksheet
-                            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-
-                            // Access data from the worksheet
-                            int rowCount = worksheet.Dimension.Rows;
-                            int colCount = worksheet.Dimension.Columns;
-
-                            DataTable dt = new DataTable();
-
-                            // Assuming the first row contains column headers
-                            for (int col = 1; col <= colCount; col++)
-                            {
-                                dt.Columns.Add(worksheet.Cells[1, col].Text);
-                            }
-
-                            // Starting from the second row to skip headers
-                            for (int row = 2; row <= rowCount; row++)
-                            {
-                                DataRow dataRow = dt.NewRow();
-
-                                for (int col = 1; col <= colCount; col++)
-                                {
-                                    dataRow[col - 1] = worksheet.Cells[row, col].Text;
-                                }
-
-                                dt.Rows.Add(dataRow);
-                            }
-
-                            // Checking column names present in excel sheet or not
-                            if (dt.Columns[0].ColumnName.Trim() == "EmpName" && dt.Columns[1].ColumnName.Trim() == "EmpDesignation" && dt.Columns[2].ColumnName.Trim() == "EmpSalary")
-                            {
-                                if (dt.Rows.Count > 0)
-                                {
-                                    // Method 1: delete data from Temp Table
-                                    // Method 2: inserting data into temp table if needed
-
-                                    //InsertExcelDataToMainTable(dt);
-
-                                    // Method 3: To display the inserted data from Temp or Main Table
-                                }
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-
+                        DataTable dt = ReadExcelFile(filePath);
+                        // Use the data (e.g., bind to GridView)
                     }
                 }
             }
         }
+
+
+        private DataTable ReadExcelFile(string filePath)
+        {
+            string connectionString = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath};Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1';";
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+
+                // Get the sheet name (you could also hardcode it if known)
+                DataTable schema = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string sheetName = schema.Rows[0]["TABLE_NAME"].ToString();
+
+                string query = $"SELECT * FROM [{sheetName}]";
+
+                using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+      
 
     }
 }
